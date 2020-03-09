b7ca3abf1c6052a27153f114d63e00733958309f
Miguel Delgadillo <jdelgadilloh87@gmail.com>
2020-03-08 23:52:51 -0600

    Update coolstore.config.js

--- web-nodejs/app/coolstore.config.js
+++ web-nodejs/app/coolstore.config.js
@@ -1,5 +1,5 @@
 var config = {
-    API_ENDPOINT: 'gw,
+    API_ENDPOINT: 'gw',
     SECURE_API_ENDPOINT: 'secure-gateway-' + process.env.SECURE_COOLSTORE_GW_SERVICE,
     SSO_ENABLED: process.env.SSO_URL ? true : false
 };
